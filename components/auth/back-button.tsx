24ee1d2de7cf59264aa0d08b1cd32d819f9677f3
Sharvari <sharvaridivekar09@gmail.com>
2024-04-08 13:25:34 -0700

    register: login and register validation with mysql

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import Link from "next/link"
+import { Button } from "../ui/button"
+
+interface BackButtonProps {
+    href: string
+    label: string
+}
+
+export const BackButton = ({ label, href } : BackButtonProps) => {
+    return (
+        <Button 
+            variant="link"
+            size="sm"
+            className="font-normal w-full"
+            asChild
+        >
+            <Link href={href}>
+                {label}
+            </Link>
+        </Button>
+    )
+}
---
+++ schemas/index.ts
@@ -0,0 +1,22 @@
+import * as z from "zod"
+
+export const LoginSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(4, {
+        message: "Password is required"
+    })
+})
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(4, {
+        message: "Password is required"
+    }),
+    name: z.string().min(3, {
+        message: "Name is required"
+    })
+})
